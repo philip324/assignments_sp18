0032576525e624329b093f886f7001ff0ab495b8
philip324 <yangp18@berkeley.edu>
2019-01-13 02:28:29 -0500

    finished proj1A

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,110 @@
+public class ArrayDeque<T> {
+    private T[] items;
+    private int size;
+    private int center;
+
+    public ArrayDeque() {
+        items = (T []) new Object[8];
+        size = 0;
+        center = 0;
+    }
+
+    private static int resizeFactor = 2;
+    private static double usageRatio = 0.25;
+
+    public void addFirst(T item) {
+        /** no looping or recursion
+         *  take constant time */
+        if (size+1 == items.length) {
+            resize(items.length*resizeFactor);
+        }
+        items[center] = item;
+        center = (center-1)%items.length;
+        size += 1;
+    }
+
+    public void addLast(T item) {
+        /** no looping or recursion
+         *  take constant time */
+        if (size+1 == items.length) {
+            resize(items.length*resizeFactor);
+        }
+        int index = (center+1+size)%items.length;
+        items[index] = item;
+        size += 1;
+    }
+
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int size() {
+        /** constant time */
+        return size;
+    }
+
+    public void printDeque() {
+        if (size > 0) {
+            System.out.print(items[center+1]);
+            int index = center+2;
+            int count = 1;
+            while (count < size) {
+                System.out.print(" ");
+                System.out.print(items[index]);
+                index += 1;
+                count += 1;
+            }
+        }
+    }
+
+    public T removeFirst() {
+        /** no looping or recursion
+         *  take constant time */
+        double ratio = (double) size/items.length;
+        if (ratio < usageRatio) {
+            resize(items.length/resizeFactor);
+        }
+        center = (center+1)%items.length;
+        T res = items[center];
+        items[center] = null;
+        size -= 1;
+        return res;
+    }
+
+    public T removeLast() {
+        /** no looping or recursion
+         *  take constant time */
+        double ratio = (double) size/items.length;
+        if (ratio < usageRatio) {
+            resize(items.length/resizeFactor);
+        }
+        int index = (center+size)%items.length;
+        T res = items[index];
+        items[index] = null;
+        size -= 1;
+        return res;
+    }
+
+    public T get(int index) {
+        /** use iteration */
+        if (index >= size) {
+            return null;
+        } else {
+            return items[(center+1+index)%items.length];
+        }
+    }
+
+    private void resize(int cap) {
+        /** change items and center */
+        T[] a = (T []) new Object[cap];
+        int copyLength = Math.min(size, items.length-center);
+        System.arraycopy(items, center, a, 0, copyLength);
+        System.arraycopy(items, 0, a, copyLength, size-copyLength);
+        center = 0;
+        items = a;
+    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,121 @@
+public class LinkedListDeque<T> {
+    public class Node {
+        public T item;
+        public Node prev;
+        public Node next;
+
+        public Node(T item, Node prev, Node next) {
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    private Node sentinel;
+    private int size;
+
+    public LinkedListDeque() {
+        sentinel = new Node(null, null, null);
+        size = 0;
+    }
+
+    public void addFirst(T item) {
+        /** no looping or recursion
+         *  take constant time */
+        Node newNode = new Node(item, sentinel, sentinel.next);
+        sentinel.next.prev = newNode;
+        sentinel.next = newNode;
+        size += 1;
+    }
+
+    public void addLast(T item) {
+        /** no looping or recursion
+         *  take constant time */
+        Node newNode = new Node(item, sentinel.prev, sentinel);
+        sentinel.prev.next = newNode;
+        sentinel.prev = newNode;
+        size += 1;
+    }
+
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int size() {
+        /** constant time */
+        return size;
+    }
+
+    public void printDeque() {
+        if (size > 0) {
+            Node pointer = sentinel.next;
+            System.out.print(pointer.item);
+            for (int i = 1; i < size; i += 1) {
+                pointer = pointer.next;
+                System.out.print(" ");
+                System.out.print(pointer.item);
+            }
+        }
+    }
+
+    public T removeFirst() {
+        /** no looping or recursion
+         *  take constant time */
+        if (size == 0) {
+            return null;
+        } else {
+            Node pointer = sentinel.next;
+            pointer.next.prev = sentinel;
+            sentinel.next = pointer.next;
+            pointer.prev = null;
+            pointer.next = null;
+            size -= 1;
+            return pointer.item;
+        }
+    }
+
+    public T removeLast() {
+        /** no looping or recursion
+         *  take constant time */
+        if (size == 0) {
+            return null;
+        } else {
+            Node pointer = sentinel.prev;
+            pointer.prev.next = sentinel;
+            sentinel.prev = pointer.prev;
+            pointer.prev = null;
+            pointer.next = null;
+            size -= 1;
+            return pointer.item;
+        }
+    }
+
+    public T get(int index) {
+        /** use iteration */
+        if (index >= size) {
+            return null;
+        } else {
+            Node pointer = sentinel.next;
+            for (int i = 0; i < index; i += 1) {
+                pointer = pointer.next;
+            }
+            return pointer.item;
+        }
+    }
+
+    public T getRecursive(int index) {
+        /** use recursion */
+        if (index >= size) {
+            return null;
+        } else if (index == 0) {
+            return sentinel.next.item;
+        } else {
+            removeFirst();
+            return getRecursive(index-1);
+        }
+    }
+}
