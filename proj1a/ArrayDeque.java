efc92e04f2b9cbbe0816c377a0eed4ce9b615cc6
philip324 <yangp18@berkeley.edu>
2019-01-13 16:18:23 -0500

    fix arrayDeque

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,36 +1,34 @@
 public class ArrayDeque<T> {
     private T[] items;
     private int size;
-    private int center;
+    private int nextFirst;
+    private int nextLast;
 
     public ArrayDeque() {
         items = (T []) new Object[8];
         size = 0;
-        center = 0;
+        nextFirst = 0;
+        nextLast = nextFirst + 1;
     }
 
     private static int resizeFactor = 2;
     private static double usageRatio = 0.25;
 
-    /** no looping or recursion
-     *  take constant time */
     public void addFirst(T item) {
         if (size + 1 == items.length) {
             resize(items.length * resizeFactor);
         }
-        items[center] = item;
-        center = (center - 1) % items.length;
+        items[nextFirst] = item;
+        nextFirst = (nextFirst - 1) % items.length;
         size += 1;
     }
 
-    /** no looping or recursion
-     *  take constant time */
     public void addLast(T item) {
         if (size + 1 == items.length) {
             resize(items.length * resizeFactor);
         }
-        int index = (center + 1 + size) % items.length;
-        items[index] = item;
+        items[nextLast] = item;
+        nextLast = (nextLast + 1) % items.length;
         size += 1;
     }
 
@@ -38,69 +36,73 @@ public class ArrayDeque<T> {
         return size == 0;
     }
 
-    /** constant time */
     public int size() {
         return size;
     }
 
     public void printDeque() {
         if (size > 0) {
-            System.out.print(items[center + 1]);
-            int index = center + 2;
+            int index = (nextFirst + 1) % items.length;
             int count = 1;
+            System.out.print(items[index]);
             while (count < size) {
+                index = (index + 1) % items.length;
                 System.out.print(" ");
                 System.out.print(items[index]);
-                index += 1;
                 count += 1;
             }
         }
     }
 
-    /** no looping or recursion
-     *  take constant time */
     public T removeFirst() {
+        nextFirst = (nextFirst + 1) % items.length;
+        T res = items[nextFirst];
+        items[nextFirst] = null;
+        size -= 1;
+
         double ratio = (double) size / items.length;
         if (ratio < usageRatio) {
-            resize(items.length / resizeFactor);
+            int newLength = Math.max(8, items.length / resizeFactor);
+            resize(newLength);
         }
-        center = (center + 1) % items.length;
-        T res = items[center];
-        items[center] = null;
-        size -= 1;
         return res;
     }
 
-    /** no looping or recursion
-     *  take constant time */
     public T removeLast() {
+        nextLast = (nextLast - 1) % items.length;
+        T res = items[nextLast];
+        items[nextLast] = null;
+        size -= 1;
+
         double ratio = (double) size / items.length;
         if (ratio < usageRatio) {
-            resize(items.length / resizeFactor);
+            int newLength = Math.max(8, items.length / resizeFactor);
+            resize(newLength);
         }
-        int index = (center + size) % items.length;
-        T res = items[index];
-        items[index] = null;
-        size -= 1;
         return res;
     }
 
-    /** use iteration */
     public T get(int index) {
         if (index >= size) {
             return null;
         } else {
-            return items[(center + 1 + index) % items.length];
+            int shiftedIndex = (nextFirst + 1 + index) % items.length;
+            return items[shiftedIndex];
         }
     }
 
-    /** change items and center */
     private void resize(int cap) {
         T[] a = (T []) new Object[cap];
-        int copyLength = Math.min(size, items.length - center);
-        System.arraycopy(items, center, a, 0, copyLength);
-        System.arraycopy(items, 0, a, copyLength, size - copyLength);
-        center = 0;
+        if (nextFirst < nextLast) {
+            System.arraycopy(items, nextFirst + 1, a, 1, size);
+        } else {
+            int l1 = items.length - (nextFirst + 1);
+            System.arraycopy(items, nextFirst + 1, a, 1, l1);
+            int l2 = size - l1;
+            System.arraycopy(items, 0, a, l1 + 1, l2);
+        }
+        nextFirst = 0;
+        nextLast = (nextFirst + size + 1) % a.length;
         items = a;
     }
 }
